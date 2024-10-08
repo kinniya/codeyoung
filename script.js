2783604b2aca8942fa3bb65b862f18dea983e6d5
kinniya <kinniya@gmail.com>
2024-10-07 21:08:26 -0400

    fear - added new files

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get the button element by its ID
+const button = document.getElementById('myButton');
+
+// Add a click event listener to the button
+button.addEventListener('click', () => {
+    alert('Hello! Now you have completed Class 01.');
+});
